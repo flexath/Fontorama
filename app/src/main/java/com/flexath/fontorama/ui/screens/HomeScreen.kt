b7d370ec11999a4691c5130210b8fa80ab11d1bb
flexath11 <flexath11@gmail.com>
2023-11-06 22:21:17 +0630

    HomeScreen still doing.

--- app/src/main/java/com/flexath/fontorama/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/flexath/fontorama/ui/screens/HomeScreen.kt
@@ -1,14 +1,16 @@
 package com.flexath.fontorama.ui.screens
 
+import android.content.ClipData
+import android.content.Context
+import android.content.Context.CLIPBOARD_SERVICE
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
@@ -30,10 +32,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.ClipboardManager
-import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -41,17 +40,20 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.exyte.animatednavbar.utils.noRippleClickable
 import com.flexath.fontorama.activities.changeColorWithDisplayMode
 import com.flexath.fontorama.ui.theme.ColorDarkGray
 import com.flexath.fontorama.ui.theme.ColorLightGray
 import com.flexath.fontorama.ui.theme.LightColorScheme
 import com.flexath.fontorama.ui.theme.RobotoCondensed
+import com.flexath.fontorama.ui.theme.fontFamily
 import com.flexath.fontorama.utils.textResultStyleList
+import timber.log.Timber
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -72,18 +74,15 @@ fun HomeScreen(
             .fillMaxSize()
             .padding(bottom = 20.dp)
     ) {
-
-        TextFieldSection()
-
-        TextResultSection()
+        HomeScreenSetUp()
     }
 }
 
 @Composable
-fun TextFieldSection() {
+fun HomeScreenSetUp() {
 
     var text by rememberSaveable {
-        mutableStateOf("")
+        mutableStateOf("Aung Thiha")
     }
 
     ElevatedCard(
@@ -193,10 +192,6 @@ fun TextFieldSection() {
                 .wrapContentHeight(),
         )
     }
-}
-
-@Composable
-fun TextResultSection() {
 
     ElevatedCard(
         elevation = CardDefaults.cardElevation(
@@ -247,58 +242,54 @@ fun TextResultSection() {
 
         ) {
             items(textResultStyleList) {
-                TextResult(fontWeight = it.fontWeight, fontFamily = it.fontFamily)
+                val context = LocalContext.current
+
+                val selectedText by remember { mutableStateOf(AnnotatedString(text)) }
+
+                SelectionContainer {
+                    Text(
+                        text = text,
+                        fontSize = 30.sp,
+                        fontFamily = it.fontFamily,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Start,
+                        lineHeight = 40.sp,
+                        color = changeColorWithDisplayMode(
+                            darkColor = Color.White,
+                            lightColor = Color.DarkGray
+                        ),
+                        modifier = Modifier
+                            .background(
+                                color = changeColorWithDisplayMode(
+                                    darkColor = ColorLightGray,
+                                    lightColor = LightColorScheme.surface
+                                )
+                            )
+                            .padding(start = 20.dp, top = 20.dp, bottom = 10.dp)
+                            .fillMaxWidth()
+                            .clickable {
+                                copyTextToClipboard(context, text)
+
+                                Toast
+                                    .makeText(context, selectedText.text, Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                    )
+                }
             }
         }
     }
 }
 
-@Composable
-fun TextResult(fontWeight: FontWeight, fontFamily: FontFamily) {
-    var newText by rememberSaveable {
-        mutableStateOf("Aung Thiha")
-    }
+private fun copyTextToClipboard(context: Context, text:String) {
+    val clipboardManager = context.getSystemService(CLIPBOARD_SERVICE) as android.content.ClipboardManager
+    val clipData = ClipData.newPlainText("text", text)
+    clipboardManager.setPrimaryClip(clipData)
 
-    val context = LocalContext.current
-
-    val selectedText by remember { mutableStateOf(AnnotatedString(newText)) }
-
-    val clipboardManager: ClipboardManager = LocalClipboardManager.current
-
-    SelectionContainer {
-        Text(
-            text = selectedText,
-            fontSize = 30.sp,
-            fontFamily = fontFamily,
-            fontWeight = fontWeight,
-            textAlign = TextAlign.Start,
-            color = changeColorWithDisplayMode(
-                darkColor = Color.White,
-                lightColor = Color.DarkGray
-            ),
-            modifier = Modifier
-                .background(
-                    color = changeColorWithDisplayMode(
-                        darkColor = ColorLightGray,
-                        lightColor = LightColorScheme.surface
-                    )
-                )
-                .padding(start = 20.dp, top = 20.dp, bottom = 10.dp)
-                .fillMaxWidth()
-                .clickable {
-                    clipboardManager.setText(selectedText)
-
-                    Toast
-                        .makeText(context, selectedText.text, Toast.LENGTH_SHORT)
-                        .show()
-                }
-        )
-    }
+    Toast.makeText(context, "Text copied to clipboard", Toast.LENGTH_LONG).show()
 }
 
 
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showSystemUi = true)
 @Composable
 fun HomeScreenPreview() {
